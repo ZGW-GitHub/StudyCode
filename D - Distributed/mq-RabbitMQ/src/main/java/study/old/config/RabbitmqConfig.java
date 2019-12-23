d8786dfc36ceb30d2a8f85689ba77d90c0f955e7
ZGW-GitHub <MrQianyan@gmail.com>
2019-12-23 23:41:01 +0800

    更改依赖

---
+++ D - Distributed/mq-RabbitMQ/src/main/java/study/old/config/RabbitmqConfig.java
@@ -0,0 +1,48 @@
+package study.old.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    public static final String QUEUE_EMAIL = "queue_email";
+    public static final String QUEUE_SMS = "queue_sms";
+    public static final String EXCHANGE_TOPICS = "exchange_topics";
+
+    /**
+     * 交换机配置
+     * @return a exchange
+     */
+    @Bean(EXCHANGE_TOPICS)
+    public Exchange EXCHANGE_TOPICS() {
+        // .durable(true)：持久化交换机
+        return ExchangeBuilder.topicExchange(EXCHANGE_TOPICS).durable(true).build();
+    }
+
+    // 声明队列
+    @Bean(QUEUE_EMAIL)
+    public Queue QUEUE_EMAIL() {
+        return new Queue(QUEUE_EMAIL);
+    }
+
+    // 声明队列
+    @Bean(QUEUE_SMS)
+    public Queue QUEUE_SMS() {
+        return new Queue(QUEUE_SMS);
+    }
+
+    /**
+     * 绑定队列与交换机
+     * @return the binding
+     */
+    public Binding EXCHANGE_QUEUE_EMAIL(@Qualifier(QUEUE_EMAIL) Queue queue, @Qualifier(EXCHANGE_TOPICS) Exchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("inform.#").noargs();
+    }
+    public Binding EXCHANGE_QUEUE_SMS(@Qualifier(QUEUE_SMS) Queue queue, @Qualifier(EXCHANGE_TOPICS) Exchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("inform.#").noargs();
+    }
+
+}
---
+++ D - Distributed/mq-RabbitMQ/src/main/java/study/old/demo01/Consumer2.java
@@ -0,0 +1,35 @@
+package study.old.demo01;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import study.old.MyRabbitMQ;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 直接通信
+ */
+public class Consumer2 {
+
+    public static void main(String[] args) throws IOException {
+
+        Channel channel = MyRabbitMQ.getChannel();
+
+        channel.queueDeclare(MyRabbitMQ.QUEUE_NAME, true, false, false, null);
+
+        com.rabbitmq.client.Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, StandardCharsets.UTF_8);
+                System.out.println("收到了：" + message);
+            }
+        };
+
+        channel.basicConsume(MyRabbitMQ.QUEUE_NAME, true, consumer);
+
+    }
+
+}
