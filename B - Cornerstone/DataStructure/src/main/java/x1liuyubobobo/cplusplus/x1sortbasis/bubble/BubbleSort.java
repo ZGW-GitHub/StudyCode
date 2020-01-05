78bb131c347f1f5c1d904883c1c7b58ddad956ae
ZGW-GitHub <MrQianyan@gmail.com>
2020-01-05 15:22:29 +0800

    排序算法

--- B - Cornerstone/DataStructure/src/main/java/x1liuyubobobo/cplusplus/x1sortbasis/bubble/BubbleSort.java
+++ B - Cornerstone/DataStructure/src/main/java/x1liuyubobobo/cplusplus/x1sortbasis/bubble/BubbleSort.java
@@ -12,21 +12,14 @@ import x1liuyubobobo.cplusplus.MySortHelper;
 public class BubbleSort {
 
     public static void sort(int[] arr) {
-
         int len = arr.length;
-
         for (int i = 0; i < len; i++) {
-
             for (int j = 0; j < len - i - 1; j++) {
-
                 if (arr[j] > arr[j + 1]) {
                     MySortHelper.swap(arr, j, j + 1);
                 }
-
             }
-
         }
-
     }
 
 }
