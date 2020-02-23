2c862f1a37301e7a125c74d52b0ec193003017f3
ZGW-GitHub <MrQianyan@gmail.com>
2020-02-23 22:59:14 +0800

    测试用例

---
+++ A - Java/src/main/java/demo/Demo.java
@@ -0,0 +1,17 @@
+package demo;
+
+/**
+ * @author NotUpToYou
+ * @date 2020/2/23 周日 15:18
+ */
+public class Demo {
+
+    int a = 2;
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
