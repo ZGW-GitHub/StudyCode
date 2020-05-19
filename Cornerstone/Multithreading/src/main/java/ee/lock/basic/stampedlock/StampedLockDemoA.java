f3f8972114e9cc5e903f3e16f0d8fd15177e7695
愆凡 <zgwmail@icloud.com>
2020-05-19 17:59:00 +0800

    多线程 - Lock

--- Cornerstone/Multithreading/src/main/java/ee/lock/basic/stampedlock/StampedLockDemoA.java
+++ Cornerstone/Multithreading/src/main/java/ee/lock/basic/stampedlock/StampedLockDemoA.java
@@ -1,9 +1,17 @@
 package ee.lock.basic.stampedlock;
 
+import java.util.concurrent.locks.StampedLock;
+
 /**
  * @author 愆凡
  * @date 2020/5/19 4:47 下午
  */
 public class StampedLockDemoA {
 
+	private static final StampedLock LOCK = new StampedLock();
+
+	public static void main(String[] args) {
+
+
+	}
 }
