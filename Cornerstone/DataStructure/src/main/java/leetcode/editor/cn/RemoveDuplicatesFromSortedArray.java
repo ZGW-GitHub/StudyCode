a782b7462a9cc5311500c9501d8ef93c9907899d
愆凡 <zgwmail@icloud.com>
2020-06-17 15:54:01 +0800

    [26]删除排序数组中的重复项

--- Cornerstone/DataStructure/src/main/java/leetcode/editor/cn/RemoveDuplicatesFromSortedArray.java
+++ Cornerstone/DataStructure/src/main/java/leetcode/editor/cn/RemoveDuplicatesFromSortedArray.java
@@ -53,12 +53,28 @@ public class RemoveDuplicatesFromSortedArray {
 	public static void main(String[] args) {
 		Solution solution = new RemoveDuplicatesFromSortedArray().new Solution();
 
+		System.out.println(solution.removeDuplicates(new int[]{1, 1, 2, 2, 3, 4, 4, 5, 5, 5, 6}));
 	}
 
 	//leetcode submit region begin(Prohibit modification and deletion)
 	class Solution {
+		/**
+		 * 双指针
+		 */
 		public int removeDuplicates(int[] nums) {
-			return 0;
+			if (nums.length == 0) {
+				return 0;
+			}
+
+			int i = 0;
+			for (int j = 1; j < nums.length; j++) {
+				if (nums[i] != nums[j]) {
+					i++;
+					nums[i] = nums[j];
+				}
+			}
+
+			return i + 1;
 		}
 	}
 	//leetcode submit region end(Prohibit modification and deletion)
