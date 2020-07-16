3cc70d98bf4e14d74829a0f87ad221967ddd0cde
愆凡 <zgwmail@icloud.com>
2020-07-16 16:59:44 +0800

    ListDemo

--- Java/src/main/java/com/code/demo/Demo.java
+++ Java/src/main/java/com/code/demo/Demo.java
@@ -1,5 +1,10 @@
 package com.code.demo;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author 愆凡
  * @date 2020/7/15 5:08 下午
@@ -7,7 +12,27 @@ package com.code.demo;
 public class Demo {
 	public static void main(String[] args) {
 
+		List<User> users = new ArrayList<>();
+		users.add(new User(12, "test"));
+		users.add(new User(13, "test"));
+		users.add(new User(6, "test"));
+		users.add(new User(2, "test"));
+		users.add(new User(3, "test"));
+		users.add(new User(16, "test"));
 
+		List<User> collect = users.stream().sorted(Comparator.comparing(User::getAge)).collect(Collectors.toList());
+
+		collect.forEach(user -> {
+			System.out.println(user.getAge());
+		});
+
+		System.out.println("---");
+
+		collect = collect.subList(4, collect.size());
+		collect.forEach(user -> {
+			System.out.println(user.getAge());
+		});
+		System.out.println(collect.size());
 
 	}
 }
---
+++ Java/src/main/java/com/code/demo/User.java
@@ -0,0 +1,21 @@
+package com.code.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author 愆凡
+ * @date 2020/7/16 2:25 下午
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+
+	private Integer age;
+	private String name;
+
+}
