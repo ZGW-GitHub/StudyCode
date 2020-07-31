69f532ea964c79008b5c0ecc967d07b51fbe917c
愆凡 <zgwmail@icloud.com>
2020-07-31 12:01:06 +0800

    springboot - 依赖修改

--- Frame/SpringBoot/data-jpa/src/main/java/com/code/data/jpa/DataJpaApplication.java
+++ Frame/SpringBoot/data-jpa/src/main/java/com/code/data/jpa/DataJpaApplication.java
@@ -1,8 +1,19 @@
 package com.code.data.jpa;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author 愆凡
  * @date 2020/7/31 11:48 上午
  */
+@SpringBootApplication
 public class DataJpaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DataJpaApplication.class, args);
+
+	}
+
 }
