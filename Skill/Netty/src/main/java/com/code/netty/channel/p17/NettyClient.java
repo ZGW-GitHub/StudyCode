3fcf8731cedca3d07cd66aa32b2e63bb608db1bd
愆凡 <zgwmail@icloud.com>
2020-08-25 17:29:54 +0800

    Skill - Netty 包结构修改

---
+++ Skill/Netty/src/main/java/com/code/netty/channel/p17/NettyClient.java
@@ -0,0 +1,39 @@
+package com.code.netty.channel.p17;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class NettyClient {
+	public static void main(String[] args) {
+
+		EventLoopGroup group = new NioEventLoopGroup();
+
+		try {
+
+			Bootstrap bootstrap = new Bootstrap()
+					.group(group)
+					.channel(NioSocketChannel.class)
+					.handler(new ChannelInitializer<SocketChannel>() {
+						@Override
+						protected void initChannel(SocketChannel channel) {
+							channel.pipeline().addLast(new ClientChannelHandler());
+						}
+					});
+
+			ChannelFuture channelFuture = bootstrap.connect("localhost", 28082).sync();
+
+			channelFuture.channel().closeFuture().sync();
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			group.shutdownGracefully();
+		}
+
+	}
+}
