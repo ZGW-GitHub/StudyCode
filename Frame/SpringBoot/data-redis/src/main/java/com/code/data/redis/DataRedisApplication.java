bd4912f42440abd79737857afd2411cca09daae6
zgw <zgw@spacetech.com.cn>
2020-08-28 16:29:16 +0800

    SpringData redis

---
+++ Frame/SpringBoot/data-redis/src/main/java/com/code/data/redis/DataRedisApplication.java
@@ -0,0 +1,19 @@
+package com.code.data.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zgw
+ * @date 2020/8/28 4:26 下午
+ */
+@SpringBootApplication
+public class DataRedisApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DataRedisApplication.class);
+
+	}
+
+}
