a36511c4c12ce9aced31f21b0b7ddb82156ce222
Snow <zgwmail@icloud.com>
2020-08-29 23:24:42 +0800

    Zookeeper

---
+++ Frame/SpringBoot/zookeeper/src/test/java/com/code/zookeeper/ZookeeperApplicationTest.java
@@ -0,0 +1,20 @@
+package com.code.zookeeper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author 愆凡
+ * @date 2020/8/29 周六 23:23
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZookeeperApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
