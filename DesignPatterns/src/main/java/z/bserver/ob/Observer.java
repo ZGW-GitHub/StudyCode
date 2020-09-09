aa3d9bef6729bf2989fc885e8c3776e7c90beb46
zgw <zgwmail@icloud.com>
2020-09-09 11:56:33 +0800

    设计模式 - 观察者模式

--- DesignPatterns/src/main/java/z/bserver/ob/Observer.java
+++
@@ -1,9 +0,0 @@
-package z.bserver.ob;
-
-/**
- * 观察者接口
- */
-public interface Observer {
-    // 更新数据方法
-    public void updata(float temp, float humidity, float pressure);
-}
---
+++ DesignPatterns/src/main/java/z/bserver/subject/SubjectDemo.java
@@ -0,0 +1,35 @@
+package z.bserver.subject;
+
+import z.bserver.observer.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 被观察者一
+ *
+ * @author 愆凡
+ * @date 2020/9/9 10:51 上午
+ */
+public class SubjectDemo implements Subject {
+
+	private List<Observer> observers = new ArrayList<>();
+
+	// 注册观察者
+	@Override
+	public void registerObserver(Observer observer) {
+		observers.add(observer);
+	}
+
+	// 移除观察者
+	@Override
+	public void deleteObserver(Observer observer) {
+		observers.remove(observer);
+	}
+
+	// 通知观察者
+	@Override
+	public void notifyObservers(String msg) {
+		observers.forEach(observer -> observer.update(msg));
+	}
+}
