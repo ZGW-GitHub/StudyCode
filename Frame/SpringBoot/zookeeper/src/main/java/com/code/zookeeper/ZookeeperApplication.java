4418987fd7bf7e0e5dcb9ff81fa8b0f0bb1ea8ba
zgw <zgwmail@icloud.com>
2020-09-14 11:48:04 +0800

    Spring Boot - Zookeeper

--- Frame/SpringBoot/zookeeper/src/main/java/com/code/zookeeper/ZookeeperApplication.java
+++ Frame/SpringBoot/zookeeper/src/main/java/com/code/zookeeper/ZookeeperApplication.java
@@ -1,8 +1,19 @@
 package com.code.zookeeper;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author 愆凡
  * @date 2020/7/15 3:42 下午
  */
+@SpringBootApplication
 public class ZookeeperApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ZookeeperApplication.class, args);
+
+	}
+
 }
