23a03dca7d739a62485d0dfd8b3f37c862091586
zgw <zgwmail@icloud.com>
2020-09-22 15:28:55 +0800

    修改

--- Java/src/main/java/com/code/collection/arraylist/Demo.java
+++ Java/src/main/java/com/code/collection/arraylist/Demo.java
@@ -1,7 +1,6 @@
 package com.code.collection.arraylist;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -11,27 +10,9 @@ import java.util.List;
 public class Demo {
 	public static void main(String[] args) {
 
-		List<Integer> integers = Arrays.asList(1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000);
+		List<String> list = new ArrayList<>(0);
+		list.add("test");
+		System.out.println(list.size());
 
-		List<Integer> push = new ArrayList<>();
-		for (Integer i : integers) {
-			push.add(i);
-
-			if (push.size() >= 5) {
-				List<Integer> dopush = new ArrayList<>(push);
-				push.clear();
-
-				doprint(dopush);
-			}
-		}
-
-		if (push.size() > 0) {
-			doprint(push);
-		}
-
-	}
-
-	private static void doprint(List<Integer> dopush) {
-		System.out.println(Arrays.toString(dopush.toArray()));
 	}
 }
