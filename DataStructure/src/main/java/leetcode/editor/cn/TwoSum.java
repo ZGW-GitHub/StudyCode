f14c720e44980be151aebb7fd98ef13c048bf61b
zgw <zgwmail@icloud.com>
2020-10-12 11:54:24 +0800

    算法 - [1]两数之和

--- DataStructure/src/main/java/leetcode/editor/cn/TwoSum.java
+++ DataStructure/src/main/java/leetcode/editor/cn/TwoSum.java
@@ -1,17 +1,17 @@
 package leetcode.editor.cn;
-//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。 
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 //
-// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。 
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
 //
-// 
 //
-// 示例: 
+//
+// 示例:
 //
 // 给定 nums = [2, 7, 11, 15], target = 9
 //
 //因为 nums[0] + nums[1] = 2 + 7 = 9
 //所以返回 [0, 1]
-// 
+//
 // Related Topics 数组 哈希表
 
 import java.util.Arrays;
@@ -38,7 +38,7 @@ public class TwoSum {
 		/**
 		 * 暴力
 		 */
-		public int[] twoSum(int[] nums, int target) {
+		public int[] one(int[] nums, int target) {
 			for (int i = 0; i < nums.length - 1; i++) {
 				for (int j = i + 1; j < nums.length; j++) {
 					if (nums[i] + nums[j] == target) {
@@ -52,8 +52,8 @@ public class TwoSum {
 		/**
 		 * 两次 Hash 法
 		 */
-		public int[] one(int[] nums, int target) {
-			Map<Integer, Integer> map = new HashMap<>();
+		public int[] two(int[] nums, int target) {
+			Map<Integer, Integer> map = new HashMap<>(0);
 
 			for (int i = 0; i < nums.length; i++) {
 				map.put(nums[i], i);
@@ -72,7 +72,7 @@ public class TwoSum {
 		/**
 		 * 一次 Hash 法
 		 */
-		public int[] two(int[] nums, int target) {
+		public int[] three(int[] nums, int target) {
 			Map<Integer, Integer> map = new HashMap<>();
 
 			for (int i = 0; i < nums.length; i++) {
