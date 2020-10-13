d92359d36d5453e3465046a667c33683a48ff4da
zgw <zgwmail@icloud.com>
2020-10-13 15:58:03 +0800

    Java8 - Stream

--- Java/src/main/java/com/code/version/java8/stream/DemoTest.java
+++ Java/src/main/java/com/code/version/java8/stream/DemoTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -14,11 +15,7 @@ import java.util.stream.Collectors;
  */
 public class DemoTest {
 
-	/**
-	 * 测试排序、截取
-	 */
-	@Test
-	public void sortTest() {
+	public List<User> initData() {
 		List<User> users = new ArrayList<>();
 		users.add(new User(12, "test"));
 		users.add(new User(13, "test"));
@@ -26,6 +23,16 @@ public class DemoTest {
 		users.add(new User(2, "test"));
 		users.add(new User(13, "test"));
 		users.add(new User(16, "test"));
+
+		return users;
+	}
+
+	/**
+	 * 测试排序、截取
+	 */
+	@Test
+	public void sortTest() {
+		List<User> users = initData();
 
 		List<User> collect = users.stream()
 				.sorted(Comparator.comparing(user -> user.getAge() * (-1)))
@@ -38,6 +45,26 @@ public class DemoTest {
 		collect = collect.subList(4, collect.size());
 		collect.forEach(user -> System.out.println(user.getAge()));
 		System.out.println(collect.size());
+	}
+
+	/**
+	 * 测试分组
+	 */
+	@Test
+	public void groupByTest() {
+		List<User> users = initData();
+
+		Map<Integer, Map<Integer, String>> userMap = users.stream()
+				.collect(Collectors.groupingBy(User::getAge, Collectors.toMap(User::getAge, User::getName, (v1, v2) -> v2)));
+
+		userMap.forEach((k, v) -> {
+			System.out.println(k);
+
+			v.forEach((k1, v1) -> {
+				System.out.println("-- " + k1);
+				System.out.println("---- " + v1);
+			});
+		});
 	}
 
 }
