c072489903ede3ff6b8d157e081b0ce92178a1a3
zgw <zgwmail@icloud.com>
2020-11-09 18:07:56 +0800

    SpringBoot - Zookeeper

---
+++ Frame/SpringBoot/zookeeper/src/test/java/com/code/zookeeper/client/ClientTest.java
@@ -0,0 +1,12 @@
+package com.code.zookeeper.client;
+
+import com.code.zookeeper.ZookeeperApplicationTest;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author 愆凡
+ * @date 2020/11/9 4:59 下午
+ */
+@Slf4j
+public class ClientTest extends ZookeeperApplicationTest {
+}
