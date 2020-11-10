e352067025377ec144274152e064815320818234
zgw <zgwmail@icloud.com>
2020-11-10 11:45:09 +0800

    SpringBoot - Zookeeper

--- Frame/SpringBoot/zookeeper/src/test/java/com/code/zookeeper/client/ClientTest.java
+++ Frame/SpringBoot/zookeeper/src/test/java/com/code/zookeeper/client/ClientTest.java
@@ -2,6 +2,12 @@ package com.code.zookeeper.client;
 
 import com.code.zookeeper.ZookeeperApplicationTest;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * @author 愆凡
@@ -9,4 +15,31 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class ClientTest extends ZookeeperApplicationTest {
+
+	public static final String ZOOKEEPER_ADDERS = "127.0.0.1:2181";
+	public static CuratorFramework client = null;
+
+	@Before
+	public void init() {
+		client = CuratorFrameworkFactory.builder()
+				.connectString(ZOOKEEPER_ADDERS)
+				.namespace("base")
+				.sessionTimeoutMs(5000)
+				.connectionTimeoutMs(5000)
+				.retryPolicy(new ExponentialBackoffRetry(1000, 3))
+				.build();
+
+		client.start();
+	}
+
+	@After
+	public void destroy() {
+		client.close();
+	}
+
+	@Test
+	public void initTest() {
+		log.info("init success !");
+	}
+
 }
