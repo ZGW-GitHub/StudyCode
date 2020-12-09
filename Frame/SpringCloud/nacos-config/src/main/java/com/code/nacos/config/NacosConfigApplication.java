e2878969f6045a0e0ec044b726e9daf5b6ff3ca5
zgw <zgwmail@icloud.com>
2020-12-09 13:45:02 +0800

    SpringCloud - Nacos

---
+++ Frame/SpringCloud/nacos-config/src/main/java/com/code/nacos/config/NacosConfigApplication.java
@@ -0,0 +1,8 @@
+package com.code.nacos.config;
+
+/**
+ * @author 愆凡
+ * @date 2020/12/9 1:37 下午
+ */
+public class NacosConfigApplication {
+}
--- Frame/SpringCloud/src/main/java/com/code/SpringCloudApplication.java
+++
@@ -1,8 +0,0 @@
-package com.code;
-
-/**
- * @author 愆凡
- * @date 2020/7/15 3:43 下午
- */
-public class SpringCloudApplication {
-}
