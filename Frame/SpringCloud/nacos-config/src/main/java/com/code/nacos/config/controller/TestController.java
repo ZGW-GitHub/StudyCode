ccd5e304faa493f3fe98310703164d14e20a2f74
zgw <zgwmail@icloud.com>
2020-12-16 16:58:02 +0800

    SpringCloud - Nacos Config

--- Frame/SpringCloud/nacos-config/src/main/java/com/code/nacos/config/controller/TestController.java
+++ Frame/SpringCloud/nacos-config/src/main/java/com/code/nacos/config/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.code.nacos.config.controller;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.context.environment.EnvironmentManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/config")
 public class TestController {
 	
+	@Autowired
+	private EnvironmentManager environmentManager;
+	
 	@Value("${age:10}")
 	private Integer age;
 	
 	@RequestMapping("/get")
 	public Integer get() {
+		System.err.println(ToStringBuilder.reflectionToString(environmentManager));
 		return age;
 	}
 	
