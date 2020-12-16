2e62619ca0ac5b2ec21b912cb0ef1376dc93b66e
zgw <zgwmail@icloud.com>
2020-12-16 17:18:29 +0800

    SpringCloud - Nacos Service

---
+++ Frame/SpringCloud/nacos-service/nacos-service-consumer/src/main/java/com/code/nacos/service/consumer/NacosServiceConsumerApplication.java
@@ -0,0 +1,17 @@
+package com.code.nacos.service.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 愆凡
+ * @date 2020/12/16 5:14 下午
+ */
+@SpringBootApplication
+public class NacosServiceConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosServiceConsumerApplication.class, args);
+	}
+
+}
