a3bbd3c9f37eceeff52417625c60c2c9f9c22e8e
zgw <zgwmail@icloud.com>
2020-12-16 16:59:12 +0800

    SpringCloud - Nacos Config

--- Frame/SpringCloud/nacos-config/src/main/java/com/code/nacos/config/controller/TestController.java
+++ Frame/SpringCloud/nacos-config/src/main/java/com/code/nacos/config/controller/TestController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/config")
 public class TestController {
-	
+
 	@Autowired
 	private EnvironmentManager environmentManager;
 	
