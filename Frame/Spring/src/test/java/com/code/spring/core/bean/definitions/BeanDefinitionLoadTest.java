f2ca1ae6831265846f2c1fba7b60ce074fe496ab
愆凡 <zgwmail@icloud.com>
2020-12-22 21:36:32 +0800

    Spring - BeanDefinitionLoad

--- Frame/Spring/src/test/java/com/code/spring/core/bean/definitions/BeanDefinitionLoadTest.java
+++ Frame/Spring/src/test/java/com/code/spring/core/bean/definitions/BeanDefinitionLoadTest.java
@@ -5,16 +5,30 @@ import com.code.spring.entity.User;
 import org.junit.Test;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.EncodedResource;
 
 /**
+ * BeanDefinition 读取、解析示例
+ *
  * @author 愆凡
  * @date 2020/12/22 20:54
  */
 public class BeanDefinitionLoadTest extends MySpringApplicationTest {
 
+	/**
+	 * 从 XML 资源中读取解析 BeanDefinition
+	 */
+	@Test
+	public void xmlTest() {
+
+	}
+
+	/**
+	 * 从 Properties 资源中读取解析 BeanDefinition
+	 */
 	@Test
 	public void propertieTest() {
 		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
@@ -35,6 +49,27 @@ public class BeanDefinitionLoadTest extends MySpringApplicationTest {
 
 		User user = beanFactory.getBean("user", User.class);
 		System.err.println(user);
+	}
+
+	@Test
+	public void AnnotatedTest() {
+		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+		// 实例化基于 Java 注解的 AnnotatedBeanDefinitionReader
+		AnnotatedBeanDefinitionReader beanDefinitionReader = new AnnotatedBeanDefinitionReader(beanFactory);
+
+		int beanDefinitionCountBeforeRegister = beanFactory.getBeanDefinitionCount();
+
+		// 注册（不要求类必须有 @Component 注解）
+		beanDefinitionReader.register(BeanDefinitionLoadTest.class);
+
+		int beanDefinitionCountAfterRegister = beanFactory.getBeanDefinitionCount();
+		System.err.println("BeanDefinition Register Count : " + (beanDefinitionCountAfterRegister - beanDefinitionCountBeforeRegister));
+
+		// 普通的 Class 注册到 Spring IoC 容器后，通常 Bean 名称为 Class 的名称且首字母小写
+		// Bean 名称的生成来自于 BeanNameGenerator ，注解 Bean 名称的生成来自于 AnnotatedBeanNameGenerator
+		BeanDefinitionLoadTest bean = beanFactory.getBean("beanDefinitionLoadTest", BeanDefinitionLoadTest.class);
+		System.err.println(bean);
 	}
 
 }
