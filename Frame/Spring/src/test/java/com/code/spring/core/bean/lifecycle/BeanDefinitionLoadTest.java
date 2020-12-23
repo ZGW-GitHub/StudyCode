abc82bfc1492914fa54e85c803a0365dac7bcae5
愆凡 <zgwmail@icloud.com>
2020-12-23 22:42:18 +0800

    Spring - Lifecycle

--- Frame/Spring/src/test/java/com/code/spring/core/bean/lifecycle/BeanDefinitionLoadTest.java
+++ Frame/Spring/src/test/java/com/code/spring/core/bean/lifecycle/BeanDefinitionLoadTest.java
@@ -1,4 +1,4 @@
-package com.code.spring.core.bean.definitions;
+package com.code.spring.core.bean.lifecycle;
 
 import com.code.spring.MySpringApplicationTest;
 import com.code.spring.entity.User;
---
+++ Frame/Spring/src/test/java/com/code/spring/core/bean/lifecycle/BeanInstantiationTest.java
@@ -0,0 +1,12 @@
+package com.code.spring.core.bean.lifecycle;
+
+/**
+ * Bean 实例化
+ *
+ * @author 愆凡
+ * @date 2020/12/23 22:41
+ */
+public class BeanInstantiationTest {
+	
+
+}
