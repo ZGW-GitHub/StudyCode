2b1145cb95f50e883a3c88cea703767f89202700
zgw <zgwmail@icloud.com>
2020-12-31 11:47:35 +0800

    MQ - RocketMQ

---
+++ Frame/SpringBoot/mq-rocketmq/src/test/java/com/code/mq/rocketmq/RocketMqApplicationTest.java
@@ -0,0 +1,20 @@
+package com.code.mq.rocketmq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author 愆凡
+ * @date 2020/12/31 11:46
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RocketMqApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
