095a4a97c809a391acec584d60f507e77152ea00
zgw <zgwmail@icloud.com>
2020-12-31 10:37:50 +0800

    SpringCloud - OpenFeign

---
+++ Frame/SpringCloud/openfeign/src/main/java/com/code/openfeign/OpenFeignApplication.java
@@ -0,0 +1,17 @@
+package com.code.openfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 愆凡
+ * @date 2020/12/31 10:36
+ */
+@SpringBootApplication
+public class OpenFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenFeignApplication.class, args);
+	}
+
+}
