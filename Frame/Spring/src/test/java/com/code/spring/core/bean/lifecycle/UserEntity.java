c9654b6381b13146dcd1d68b708635da0e78d547
愆凡 <zgwmail@icloud.com>
2021-01-02 16:39:23 +0800

    Spring - Lifecycle

--- Frame/Spring/src/test/java/com/code/spring/core/bean/lifecycle/UserEntity.java
+++ Frame/Spring/src/test/java/com/code/spring/core/bean/lifecycle/UserEntity.java
@@ -1,15 +1,17 @@
 package com.code.spring.core.bean.lifecycle;
 
 import com.code.spring.entity.User;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 /**
  * @author 愆凡
  * @date 2020/12/30 20:47
  */
-public class UserEntity extends User implements InitializingBean {
+public class UserEntity extends User implements InitializingBean, DisposableBean {
 
 	/**
 	 * 依赖于注解驱动，当使用 BeanFactory 启动上下文时并不会回调该方法。<br />
@@ -26,6 +28,24 @@ public class UserEntity extends User implements InitializingBean {
 	}
 
 	public void init() {
+
+	}
+
+	/**
+	 * 同样依赖于注解驱动，当使用 BeanFactory 启动上下文时并不会回调该方法。<br />
+	 * 解决：通过 beanFactory.addBeanPostProcessor(new CommonAnnotationBeanPostProcessor()) 解决。
+	 */
+	@PreDestroy
+	public void destroyPreDestroy() {
+
+	}
+
+	@Override
+	public void destroy() throws Exception {
+
+	}
+
+	public void destroyDemo() {
 
 	}
 
