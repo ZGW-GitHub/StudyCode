d74e3d1bf273e079c7aff850e803c041f855a966
愆凡 <zgwmail@icloud.com>
2021-01-03 22:50:59 +0800

    Spring - Configuration

---
+++ Frame/Spring/src/test/java/com/code/spring/configuration/SpringConfigurationTest.java
@@ -0,0 +1,50 @@
+package com.code.spring.configuration;
+
+import com.code.spring.MySpringApplicationTest;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.Map;
+
+/**
+ * Spring 外部化配置示例
+ *
+ * @author 愆凡
+ * @date 2021/1/3 22:43
+ */
+@PropertySource("classpath:/META-INF/configuration/springConfiguration.properties")
+public class SpringConfigurationTest extends MySpringApplicationTest {
+
+	@Bean
+	public ConfigurationUser configurationUser(@Value("${user.id}") Long id, @Value("${user.name}") String name) {
+		ConfigurationUser user = new ConfigurationUser();
+		user.setId(id);
+		user.setName(name);
+		return user;
+	}
+
+	/**
+	 * 基于 Java 注解的 Spring 外部化配置测试
+	 */
+	@Test
+	public void AnnotationTest() {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
+		// 注册当前类作为 Configuration Class
+		context.register(SpringConfigurationTest.class);
+
+		// 启动 Spring 应用上下文
+		context.refresh();
+
+		Map<String, ConfigurationUser> beans = context.getBeansOfType(ConfigurationUser.class);
+		beans.forEach((k, v) -> System.err.println("User id : " + v.getId() + "，User name : " + v.getName()));
+		
+
+		// 关闭 Spring 应用上下文
+		context.close();
+	}
+
+}
