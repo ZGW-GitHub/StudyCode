eaa2cc224208180389eafccb0d2c0f89f390e2eb
zgw <zgwmail@icloud.com>
2021-01-06 16:49:44 +0800

    Java - Thread Utils - CyclicBarrier

--- Java/src/main/java/com/code/thread/ee/lock/utils/cc/cyclicbarrier/CyclicBarrierTest.java
+++ Java/src/main/java/com/code/thread/ee/lock/utils/cc/cyclicbarrier/CyclicBarrierTest.java
@@ -7,43 +7,45 @@ import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * API：
+ * <br />{@link CyclicBarrier#await()} await()} —— 相互等待
+ * <br />{@link CyclicBarrier#reset()} reset()} ——
+ *
  * @author 愆凡
  */
 public class CyclicBarrierTest {
 
 	@Test
-	public void test() throws BrokenBarrierException, InterruptedException {
+	public void awaitTest() throws BrokenBarrierException, InterruptedException {
 
 		final CyclicBarrier barrier = new CyclicBarrier(3,
-				() -> System.err.println(Thread.currentThread().getName() + " 回调函数！"));
-
-		System.err.println("main start !");
-
-		new Thread(() -> {
-			try {
-				System.err.println("Thread 1 start !");
-				TimeUnit.MILLISECONDS.sleep(2_000);
-				barrier.await();
-				System.err.println("Thread 1 over !");
-			} catch (InterruptedException | BrokenBarrierException e) {
-				e.printStackTrace();
-			}
-		}).start();
+				// 最后一个完成的线程将执行该回调函数
+				() -> System.err.println(Thread.currentThread().getName() + " 执行回调函数！"));
 
 		new Thread(() -> {
 			try {
-				System.err.println("Thread 2 start !");
 				TimeUnit.MILLISECONDS.sleep(3_000);
-				barrier.await();
-				System.err.println("Thread 2 over !");
+				System.err.println(Thread.currentThread().getName() + " awaiting ...");
+				barrier.await(); // 相互等待
 			} catch (InterruptedException | BrokenBarrierException e) {
 				e.printStackTrace();
 			}
-		}).start();
+		}, "T1").start();
 
-		barrier.await();
+		new Thread(() -> {
+			try {
+				TimeUnit.MILLISECONDS.sleep(2_000);
+				System.err.println(Thread.currentThread().getName() + " awaiting ...");
+				barrier.await(); // 相互等待
+			} catch (InterruptedException | BrokenBarrierException e) {
+				e.printStackTrace();
+			}
+		}, "T2").start();
 
-		System.err.println("main over !");
+		System.err.println(Thread.currentThread().getName() + " awaiting ...");
+		barrier.await(); // 相互等待
+
+		System.err.println("over !");
 	}
 
 }
