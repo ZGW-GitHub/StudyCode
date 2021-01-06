16ef48cd8e082086c81f40b1609a1cfb58b7f2c3
zgw <zgwmail@icloud.com>
2021-01-06 17:51:02 +0800

    Java - Thread Utils - CyclicBarrier

--- Java/src/main/java/com/code/thread/ee/lock/utils/cc/cyclicbarrier/CyclicBarrierTest.java
+++ Java/src/main/java/com/code/thread/ee/lock/utils/cc/cyclicbarrier/CyclicBarrierTest.java
@@ -40,6 +40,23 @@ public class CyclicBarrierTest {
 	}
 
 	@Test
+	public void awaitTest2() throws BrokenBarrierException, InterruptedException {
+		final CyclicBarrier barrier = new CyclicBarrier(3, () -> System.err.println(Thread.currentThread().getName() + " 执行回调函数！"));
+
+		startNewThread(barrier, 3, "T1");
+		startNewThread(barrier, 2, "T2");
+
+		System.err.println(Thread.currentThread().getName() + " awaiting ...");
+		barrier.await();
+
+		startNewThread(barrier, 3, "T3");
+		startNewThread(barrier, 2, "T4");
+		barrier.await();
+
+		System.err.println("over !");
+	}
+
+	@Test
 	public void resetTest() {
 
 	}
@@ -47,9 +64,9 @@ public class CyclicBarrierTest {
 	private void startNewThread(CyclicBarrier barrier, int sleepSeconds, String threadName) {
 		new Thread(() -> {
 			try {
-				TimeUnit.SECONDS.sleep(sleepSeconds);
-
 				System.err.println(Thread.currentThread().getName() + " awaiting ...");
+
+				TimeUnit.SECONDS.sleep(sleepSeconds);
 
 				barrier.await(); // 相互等待
 			} catch (InterruptedException | BrokenBarrierException e) {
