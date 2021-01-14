5d58fe6c63a0dda004d32c6b223751541a08a1f2
zgw <zgwmail@icloud.com>
2021-01-14 18:07:32 +0800

    SpringBoot - RocketMQ

--- Frame/SpringBoot/mq-rocketmq/src/test/java/com/code/mq/rocketmq/producer/MsgTypeTest.java
+++ Frame/SpringBoot/mq-rocketmq/src/test/java/com/code/mq/rocketmq/producer/MsgTypeTest.java
@@ -1,9 +1,15 @@
 package com.code.mq.rocketmq.producer;
 
 import com.code.mq.rocketmq.RocketMqApplicationTest;
+import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * 四种消息类型：广播消息、顺序消息、定时消息、事务消息
@@ -22,6 +28,7 @@ public class MsgTypeTest extends RocketMqApplicationTest {
 	/**
 	 * 广播消息
 	 */
+	@Test
 	public void broadcastingTest() {
 
 	}
@@ -29,6 +36,7 @@ public class MsgTypeTest extends RocketMqApplicationTest {
 	/**
 	 * 顺序消息
 	 */
+	@Test
 	public void orderTest() {
 
 	}
@@ -36,13 +44,20 @@ public class MsgTypeTest extends RocketMqApplicationTest {
 	/**
 	 * 定时消息
 	 */
+	@Test
 	public void timeTest() {
+		// 组装消息
+		Message<String> message = MessageBuilder.withPayload("定时消息").build();
+		// 发送定时消息，延迟级别为3，即：10秒
+		SendResult sendResult = rocketMQTemplate.syncSend(topic, message, TimeUnit.MINUTES.toMillis(3), 3);
 
+		System.err.println(sendResult);
 	}
 
 	/**
 	 * 事务消息
 	 */
+	@Test
 	public void transactionalTest() {
 
 	}
