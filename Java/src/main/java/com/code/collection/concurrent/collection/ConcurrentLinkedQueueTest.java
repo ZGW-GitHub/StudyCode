0710c7e1603b1c3184c161d3a8f62fc0f069e971
zgw <zgwmail@icloud.com>
2021-01-18 10:52:28 +0800

    Java - Collection ConcurrentLinkedQueue

--- Java/src/main/java/com/code/collection/concurrent/collection/ConcurrentLinkedQueueTest.java
+++ Java/src/main/java/com/code/collection/concurrent/collection/ConcurrentLinkedQueueTest.java
@@ -1,5 +1,7 @@
 package com.code.collection.concurrent.collection;
 
+import org.junit.Test;
+
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
@@ -7,43 +9,15 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  */
 public class ConcurrentLinkedQueueTest {
 
-	public static void main(String[] args) {
+	private final ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<>();
 
-		ConcurrentLinkedQueue<Object> queue = new ConcurrentLinkedQueue<>();
+	@Test
+	public void test() {
+		queue.add("aaa");
+		queue.add("bbb");
+		queue.add("ccc");
 
-		for (int i = 0; i < 100000; i++) {
-			queue.add(System.nanoTime());
-		}
-
-		System.out.println("放入完成。");
-
-		long millis = System.currentTimeMillis();
-
-        /*while (queue.size() > 0) {
-            queue.poll(); // 13758 ms
-        }*/
-
-		while (!queue.isEmpty()) {
-			queue.poll(); // 4 ms       因为：isEmpty 是判断头是否有元素，size 是遍历计算 queue 大小
-		}
-
-		System.out.println(System.currentTimeMillis() - millis);
-
-	}
-
-	// 判断字符串是否存在
-	private static void handText(String s) {
-
-		if (null != s && "".equals(s)) {
-			// 低效（ 底层使用的循环 ）
-		}
-		if (null != s && s.length() > 0) {
-			// 高效
-		}
-		if (null != s && !s.isEmpty()) {
-			// 高效（ 底层调用的 s.length() ）
-		}
-
+		queue.forEach(System.err::println);
 	}
 
 }
