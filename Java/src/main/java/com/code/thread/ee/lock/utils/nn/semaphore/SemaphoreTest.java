fc2b79dc92ac68e40ae744d896d5a68e0bde3b37
zgw <zgwmail@icloud.com>
2021-01-21 15:37:03 +0800

    Java - Thread Utils - Semaphore

--- Java/src/main/java/com/code/thread/ee/lock/utils/nn/semaphore/SemaphoreTest.java
+++ Java/src/main/java/com/code/thread/ee/lock/utils/nn/semaphore/SemaphoreTest.java
@@ -1,58 +1,77 @@
 package com.code.thread.ee.lock.utils.nn.semaphore;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
-// 使用单个信号量自定义定义锁
+/**
+ * API：
+ * <br />{@link Semaphore#acquire(int) acquire(int)} —— 从信号量获取给定数量的许可证，会阻塞直至许可证获取成功
+ * <br />{@link Semaphore#release(int) acquire(int)} —— 释放给定数量的许可证
+ *
+ * @author 愆凡
+ */
+@Slf4j
 public class SemaphoreTest {
 
-	private static final DemoSemaphoreLock lockDemo = new DemoSemaphoreLock();
+	private final Semaphore semaphore = new Semaphore(2);
 
-	public static void main(String[] args) {
-
-		new Thread(() -> {
-			try {
-				lockDemo.lock();
-				System.out.println(Thread.currentThread().getName() + " : I get the lock !");
-				TimeUnit.SECONDS.sleep(3);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} finally {
-				lockDemo.unlock();
-				System.out.println(Thread.currentThread().getName() + " : the lock is free !");
-			}
-		}, "==A==").start();
-
-		new Thread(() -> {
-			try {
-				lockDemo.lock();
-				System.out.println(Thread.currentThread().getName() + " : I get the lock !");
-				TimeUnit.SECONDS.sleep(2);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} finally {
-				lockDemo.unlock();
-				System.out.println(Thread.currentThread().getName() + " : the lock is free !");
-			}
-		}, "==B==").start();
-
-	}
+	// 标识是否有已生产的数据
+	private volatile boolean isHave = false;
+	// 用来存储生产的数据
+	private int num = 0;
 
 	/**
-	 * 自定义锁
+	 * 一个生产者、消费者示例
 	 */
-	static class DemoSemaphoreLock {
+	@Test
+	public void consumerTest() throws InterruptedException {
+		new Thread(this::provider, "T1").start();
+		new Thread(this::consumer, "T2").start();
 
-		private final Semaphore semaphore = new Semaphore(1);
+		Thread.currentThread().join();
+	}
 
-		void lock() throws InterruptedException {
-			semaphore.acquire();
+	private void provider() {
+		while (true) {
+			try {
+				semaphore.acquire(2); // 从信号量获取许可证，获取不到阻塞在这里
+				if (isHave) {
+					semaphore.release(2);  // 释放许可证
+					TimeUnit.SECONDS.sleep(1);
+				} else {
+					TimeUnit.SECONDS.sleep(1);
+					System.err.println(Thread.currentThread().getName() + "，生产了 ：" + ++num);
+
+					isHave = true;
+					semaphore.release(2); // 释放许可证
+				}
+			} catch (InterruptedException e) {
+				log.error("生产者发生异常 : ", e);
+			}
 		}
+	}
 
-		void unlock() {
-			semaphore.release();
+	private void consumer() {
+		while (true) {
+			try {
+				semaphore.acquire(2);
+				if (!isHave) {
+					semaphore.release(2);
+					TimeUnit.SECONDS.sleep(1);
+				} else {
+					TimeUnit.SECONDS.sleep(1);
+					System.err.println(Thread.currentThread().getName() + "，消费了 ：" + num);
+
+					isHave = false;
+					semaphore.release(2);
+				}
+			} catch (InterruptedException e) {
+				log.error("消费者发生异常 : ", e);
+			}
 		}
-
 	}
 
 }
