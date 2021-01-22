fc5a51711b8f542efe5737aaeb7283c750f25724
zgw <zgwmail@icloud.com>
2021-01-22 17:07:12 +0800

    Java - Thread Utils - Phaser

--- Java/src/main/java/com/code/thread/ee/lock/utils/ee/phaser/PhaserTest.java
+++ Java/src/main/java/com/code/thread/ee/lock/utils/ee/phaser/PhaserTest.java
@@ -7,12 +7,6 @@ import java.util.concurrent.Phaser;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 带参构造函数
- * 参数：
- * int ：
- * 使 phaser 包含给定数量的已注册未到达的参与方
- * 同时也规定了进入下一阶段所需的已到达参与方数量
- *
  * @author 愆凡
  */
 @Slf4j
