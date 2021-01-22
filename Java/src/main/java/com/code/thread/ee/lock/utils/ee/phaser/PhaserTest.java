3ff164816be4db5b2bdcad04dcbaf22f021461c1
zgw <zgwmail@icloud.com>
2021-01-22 17:22:12 +0800

    Java - Thread Utils - Phaser

--- Java/src/main/java/com/code/thread/ee/lock/utils/ee/phaser/PhaserTest.java
+++ Java/src/main/java/com/code/thread/ee/lock/utils/ee/phaser/PhaserTest.java
@@ -7,6 +7,11 @@ import java.util.concurrent.Phaser;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * API：
+ * <br />{@link Phaser#getPhase() getPhase()} —— 获取当前阶段
+ * <br />{@link Phaser#register() register()} —— 注册一个参与者
+ * <br />{@link Phaser#arriveAndAwaitAdvance() arriveAndAwaitAdvance()} —— 等待其它参与者到达
+ *
  * @author 愆凡
  */
 @Slf4j
@@ -20,9 +25,30 @@ public class PhaserTest {
 		startNewThread(phaser, 2, "T2");
 		startNewThread(phaser, 3, "T3");
 
+		System.err.println(phaser.getPhase()); // 获取当前阶段
 
 		phaser.register(); // 注册一个参与者
 		phaser.arriveAndAwaitAdvance(); // 等待其它参与者到达
+
+		System.err.println(phaser.getPhase()); // 获取当前阶段
+
+		System.err.println("over !");
+	}
+
+	@Test
+	public void baseTest2() {
+		final Phaser phaser = new Phaser();
+
+		startNewThread(phaser, 1, "T1");
+		startNewThread(phaser, 2, "T2");
+		startNewThread(phaser, 3, "T3");
+
+		System.err.println(phaser.getPhase()); // 获取当前阶段
+
+//		phaser.register(); // 注册一个参与者
+		phaser.arriveAndAwaitAdvance(); // 等待其它参与者到达
+
+		System.err.println(phaser.getPhase()); // 获取当前阶段
 
 		System.err.println("over !");
 	}
