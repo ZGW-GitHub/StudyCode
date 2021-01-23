7dab0f9f55fe4b58d2f9c9802bec4f84d1c1ae12
愆凡 <zgwmail@icloud.com>
2021-01-23 22:57:15 +0800

    Spring - EventPublisher

---
+++ Frame/Spring/src/test/java/com/code/spring/event/SpringEventMulticasterTest.java
@@ -0,0 +1,50 @@
+package com.code.spring.event;
+
+import com.code.spring.MySpringApplicationTest;
+import org.junit.Test;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.support.GenericApplicationContext;
+
+/**
+ * Spring 事件广播器（发布器）示例
+ *
+ * @see ApplicationEventPublisher
+ * @see ApplicationEventPublisherAware
+ *
+ * @author 愆凡
+ * @date 2021/1/23 22:42
+ */
+@SuppressWarnings("all")
+public class SpringEventMulticasterTest extends MySpringApplicationTest implements ApplicationEventPublisherAware {
+
+	@Test
+	public void publisherTest() {
+		GenericApplicationContext context = new GenericApplicationContext();
+
+		// 注册当前类为 Spring Bean 以实现 ApplicationEventPublisher 的注入
+		context.registerBean(SpringEventMulticasterTest.class);
+
+		// 注册 Spring 事件监听器
+		context.addApplicationListener(new ApplicationListener<ApplicationEvent>() {
+			@Override
+			public void onApplicationEvent(ApplicationEvent event) {
+				System.err.println(event.toString());
+			}
+		});
+
+		context.refresh();
+	}
+
+	// 通过 Spring 调用 set 方法时来发布一个事件
+	@Override
+	public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
+		publisher.publishEvent(new ApplicationEvent("Hello World !") {
+		});
+
+		publisher.publishEvent("Hello World !!!");
+	}
+
+}
