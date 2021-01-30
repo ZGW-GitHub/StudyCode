910cfdf12970e3a5dc5499ca48c1057bb3979d95
愆凡 <zgwmail@icloud.com>
2021-01-30 22:59:22 +0800

    Spring - IoC - 依赖管理：依赖注入

---
+++ Frame/Spring/src/test/java/com/code/spring/ioc/dependency/injection/DependencyInjectionTest.java
@@ -0,0 +1,14 @@
+package com.code.spring.ioc.dependency.injection;
+
+import com.code.spring.MySpringApplicationTest;
+
+/**
+ * 依赖管理：依赖注入示例
+ *
+ * @author 愆凡
+ * @date 2021/1/30 22:57
+ */
+public class DependencyInjectionTest extends MySpringApplicationTest {
+	
+
+}
