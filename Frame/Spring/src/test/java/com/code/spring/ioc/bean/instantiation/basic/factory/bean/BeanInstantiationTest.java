69c684c23ac0aa05e33de179418ff5ec0514a73a
愆凡 <zgwmail@icloud.com>
2021-01-31 23:00:36 +0800

    Spring - Bean - 实例化方式

---
+++ Frame/Spring/src/test/java/com/code/spring/ioc/bean/instantiation/basic/factory/bean/BeanInstantiationTest.java
@@ -0,0 +1,26 @@
+package com.code.spring.ioc.bean.instantiation.basic.factory.bean;
+
+import com.code.spring.MySpringApplicationTest;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Spring Bean 的实例化方式 ：通过静态工厂方法
+ *
+ * @author 愆凡
+ * @date 2021/1/31 22:04
+ */
+public class BeanInstantiationTest extends MySpringApplicationTest {
+
+	@Test
+	public void byXmlTest() {
+		BeanFactory beanFactory = new ClassPathXmlApplicationContext(
+				"classpath:/META-INF/ioc/bean/instantiation/bean-instantiation-factory-bean.xml");
+
+		TestUser user = beanFactory.getBean("userFactoryBean", TestUser.class);
+
+		System.err.println(user);
+	}
+
+}
