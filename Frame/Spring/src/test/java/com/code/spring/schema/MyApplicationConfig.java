546fd5363545a2d74edd39417389f786b34e10ca
zgw <zgwmail@icloud.com>
2021-02-03 13:45:24 +0800

    Spring - 扩展 - Schema

---
+++ Frame/Spring/src/test/java/com/code/spring/schema/MyApplicationConfig.java
@@ -0,0 +1,18 @@
+package com.code.spring.schema;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author 愆凡
+ * @date 2021/2/3 10:46
+ */
+@Data
+@ToString
+public class MyApplicationConfig {
+
+	private String appName;
+	private String address;
+	private Integer port;
+
+}
---
+++ Frame/Spring/src/test/java/com/code/spring/schema/SchemaTest.java
@@ -0,0 +1,22 @@
+package com.code.spring.schema;
+
+import com.code.spring.MySpringApplicationTest;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author 愆凡
+ * @date 2021/2/3 10:46
+ */
+public class SchemaTest extends MySpringApplicationTest {
+
+	@Test
+	public void test() {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:/META-INF/test.xml");
+
+		MyApplicationConfig config = context.getBean(MyApplicationConfig.class);
+
+		System.err.println(config);
+	}
+
+}
