6b8757586f38244613fb167a50081fb705e4c11c
愆凡 <zgwmail@icloud.com>
2021-02-04 21:58:22 +0800

    Spring - IoC - 依赖管理：依赖查找

--- Frame/Spring/src/test/java/com/code/spring/ioc/dependency/injection/DependencyInjectionTest.java
+++ Frame/Spring/src/test/java/com/code/spring/ioc/dependency/injection/DependencyInjectionTest.java
@@ -1,6 +1,11 @@
 package com.code.spring.ioc.dependency.injection;
 
 import com.code.spring.MySpringApplicationTest;
+import org.junit.Test;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * 依赖管理：依赖注入示例
@@ -9,6 +14,48 @@ import com.code.spring.MySpringApplicationTest;
  * @date 2021/1/30 22:57
  */
 public class DependencyInjectionTest extends MySpringApplicationTest {
-	
+
+	/**
+	 * 基于 XML 的 Setter 注入示例
+	 */
+	@Test
+	public void setterByXmlTest() {
+		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+		XmlBeanDefinitionReader definitionReader = new XmlBeanDefinitionReader(beanFactory);
+
+		String resoucePath = "classpath:/META-INF/ioc/dependency/injection/dependency-injection.xml";
+		definitionReader.loadBeanDefinitions(resoucePath);
+
+		DependencyUserHolder userHolder = beanFactory.getBean(DependencyUserHolder.class);
+
+		System.err.println(userHolder);
+	}
+
+	/**
+	 * 基于 API 的 Setter 注入示例
+	 */
+	@Test
+	public void setterByAnnotationTest() {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+
+		XmlBeanDefinitionReader definitionReader = new XmlBeanDefinitionReader(applicationContext);
+
+		String resoucePath = "classpath:/META-INF/ioc/dependency/injection/dependency-injection.xml";
+		definitionReader.loadBeanDefinitions(resoucePath);
+
+		// 构建 BeanDefinition
+		BeanDefinitionBuilder definitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(DependencyUserHolder.class);
+		definitionBuilder.addPropertyReference("user", "user");
+
+		// 注册 BeanDefinition
+		applicationContext.registerBeanDefinition("userHolderByApi", definitionBuilder.getBeanDefinition());
+
+		applicationContext.refresh();
+
+		DependencyUserHolder userHolder = applicationContext.getBean("userHolderByApi", DependencyUserHolder.class);
+
+		System.err.println(userHolder);
+	}
 
 }
---
+++ Frame/Spring/src/test/java/com/code/spring/ioc/dependency/injection/DependencyUserHolder.java
@@ -0,0 +1,15 @@
+package com.code.spring.ioc.dependency.injection;
+
+import com.code.spring.ioc.dependency.DependencyUser;
+import lombok.Data;
+
+/**
+ * @author 愆凡
+ * @date 2021/2/4 21:26
+ */
+@Data
+public class DependencyUserHolder {
+
+	private DependencyUser user;
+
+}
