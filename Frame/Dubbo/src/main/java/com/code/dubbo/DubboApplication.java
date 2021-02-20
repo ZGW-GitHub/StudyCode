5f31828a3410b54a8fdc76c715f74af466b8b6e9
zgw <zgwmail@icloud.com>
2021-02-20 11:30:32 +0800

    Dubbo

---
+++ Frame/Dubbo/src/main/java/com/code/dubbo/DubboApplication.java
@@ -0,0 +1,19 @@
+package com.code.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 愆凡
+ * @date 2021/2/20 11:26
+ */
+@SpringBootApplication
+public class DubboApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DubboApplication.class, args);
+
+	}
+
+}
---
+++ Frame/Dubbo/src/test/java/com/code/dubbo/DubboApplicationTest.java
@@ -0,0 +1,20 @@
+package com.code.dubbo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author 愆凡
+ * @date 2021/2/20 11:26
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DubboApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
