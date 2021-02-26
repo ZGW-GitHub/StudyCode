8a902ed3008e95bd51ac0159b6344bd113418cc1
zgw <zgw@spacetech.com.cn>
2021-02-26 18:02:45 +0800

    Spring Cloud - Gateway

---
+++ Frame/SpringCloud/gateway/src/main/java/com/code/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.code.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 愆凡
+ * @date 2021/2/26 17:52
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+}
