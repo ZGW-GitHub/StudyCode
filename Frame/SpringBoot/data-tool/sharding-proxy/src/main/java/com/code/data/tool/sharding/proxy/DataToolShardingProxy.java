e9048ceed3d8c1e05ef884ffd6b83cb96544549c
zgw <zgwmail@icloud.com>
2021-03-08 17:28:36 +0800

    DataTool - Sharding-Proxy

---
+++ Frame/SpringBoot/data-tool/sharding-proxy/src/main/java/com/code/data/tool/sharding/proxy/DataToolShardingProxy.java
@@ -0,0 +1,17 @@
+package com.code.data.tool.sharding.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 愆凡
+ * @date 2021/3/8 17:27
+ */
+@SpringBootApplication
+public class DataToolShardingProxy {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataToolShardingProxy.class, args);
+	}
+	
+}
