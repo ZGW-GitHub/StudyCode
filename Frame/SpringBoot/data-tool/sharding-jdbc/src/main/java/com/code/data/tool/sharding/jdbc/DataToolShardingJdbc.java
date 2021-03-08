2606295b5969e0f37a5f80684412736d76c5db27
zgw <zgwmail@icloud.com>
2021-03-08 17:26:37 +0800

    DataTool - Sharding-JDBC

---
+++ Frame/SpringBoot/data-tool/sharding-jdbc/src/main/java/com/code/data/tool/sharding/jdbc/DataToolShardingJdbc.java
@@ -0,0 +1,17 @@
+package com.code.data.tool.sharding.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 愆凡
+ * @date 2021/3/8 17:24
+ */
+@SpringBootApplication
+public class DataToolShardingJdbc {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataToolShardingJdbc.class, args);
+	}
+	
+}
