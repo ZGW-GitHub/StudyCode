b4a2877ce9d741c4fedbefaeed19f550d4204f11
zgw <zgwmail@icloud.com>
2021-03-09 16:36:16 +0800

    DataTool - Sharding-JDBC

---
+++ Frame/SpringBoot/data-tool/sharding-jdbc/src/test/java/com/code/data/tool/sharding/jdbc/repository/UserRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.code.data.tool.sharding.jdbc.repository;
+
+import com.code.data.tool.sharding.jdbc.DataToolShardingJdbcApplicationTest;
+import com.code.data.tool.sharding.jdbc.entity.User;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * @author 愆凡
+ * @date 2021/3/9 16:33
+ */
+public class UserRepositoryTest extends DataToolShardingJdbcApplicationTest {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Test
+	public void test() {
+		List<User> userList = userRepository.findAll();
+
+		System.err.println(userList.size());
+	}
+	
+}
