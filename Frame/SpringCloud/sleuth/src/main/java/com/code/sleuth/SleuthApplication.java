baf0a4b3918e58aab4c19b39b85ea873de418328
zgw <zgwmail@icloud.com>
2021-03-10 17:24:37 +0800

    Spring Cloud Sleuth

---
+++ Frame/SpringCloud/sleuth/src/main/java/com/code/sleuth/SleuthApplication.java
@@ -0,0 +1,17 @@
+package com.code.sleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 愆凡
+ * @date 2021/3/10 17:22
+ */
+@SpringBootApplication
+public class SleuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthApplication.class, args);
+	}
+
+}
