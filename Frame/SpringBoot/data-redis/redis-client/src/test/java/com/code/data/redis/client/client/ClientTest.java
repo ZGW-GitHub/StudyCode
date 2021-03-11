1ed8a1d483ae0f0912d306eb65cfe2f776db92da
zgw <zgwmail@icloud.com>
2021-03-11 16:07:20 +0800

    Spring Boot - Data - Redis

---
+++ Frame/SpringBoot/data-redis/redis-client/src/test/java/com/code/data/redis/client/client/ClientTest.java
@@ -0,0 +1,12 @@
+package com.code.data.redis.client.client;
+
+import com.code.data.redis.client.DataRedisClientApplicationTest;
+
+/**
+ * @author 愆凡
+ * @date 2021/3/11 16:06
+ */
+public class ClientTest extends DataRedisClientApplicationTest {
+	
+
+}
