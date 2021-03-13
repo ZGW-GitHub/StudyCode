d24fa8e9d6277d9dbf181b457030726668905d3c
zgw <zgwmail@icloud.com>
2021-03-13 15:35:17 +0800

    Spring Boot Data - Redis

--- Frame/SpringBoot/data-redis/redis-client/src/test/java/com/code/data/redis/client/test/LockTest.java
+++ Frame/SpringBoot/data-redis/redis-client/src/test/java/com/code/data/redis/client/test/LockTest.java
@@ -1,10 +1,22 @@
 package com.code.data.redis.client.test;
 
 import com.code.data.redis.client.DataRedisClientApplicationTest;
+import org.junit.Test;
 
 /**
  * @author 愆凡
  * @date 2021/3/12 09:06
  */
 public class LockTest extends DataRedisClientApplicationTest {
+
+	@Test
+	public void overTest() {
+
+	}
+
+	@Test
+	public void testOne() {
+
+	}
+
 }
