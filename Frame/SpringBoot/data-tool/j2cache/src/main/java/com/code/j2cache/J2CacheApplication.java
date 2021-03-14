3019a49b46956c15a12a902676d6ef7aa38e62a6
愆凡 <zgwmail@icloud.com>
2021-03-14 14:47:55 +0800

    DataTool - J2Cache

---
+++ Frame/SpringBoot/data-tool/j2cache/src/main/java/com/code/j2cache/J2CacheApplication.java
@@ -0,0 +1,19 @@
+package com.code.j2cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Java 两级缓存框架
+ *
+ * @author 愆凡
+ * @date 2021/3/14 14:44
+ */
+@SpringBootApplication
+public class J2CacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2CacheApplication.class, args);
+	}
+
+}
---
+++ Frame/SpringBoot/data-tool/j2cache/src/test/java/com/code/j2cache/J2CacheApplicationTest.java
@@ -0,0 +1,20 @@
+package com.code.j2cache;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author 愆凡
+ * @date 2021/3/14 14:46
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class J2CacheApplicationTest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2CacheApplicationTest.class, args);
+	}
+
+}
