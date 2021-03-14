ebce6b819ab99a7efc720185056a593f941e1fd2
愆凡 <zgwmail@icloud.com>
2021-03-14 14:55:41 +0800

    Data - Ehcache

---
+++ Frame/SpringBoot/data-ehcache/src/main/java/com/code/data/ehcache/EhcacheApplication.java
@@ -0,0 +1,19 @@
+package com.code.data.ehcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 缓存框架 Ehcache
+ *
+ * @author 愆凡
+ * @date 2021/3/14 14:51
+ */
+@SpringBootApplication
+public class EhcacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhcacheApplication.class, args);
+	}
+
+}
---
+++ Frame/SpringBoot/data-ehcache/src/test/java/com/code/data/ehcache/EhcacheApplicationTest.java
@@ -0,0 +1,20 @@
+package com.code.data.ehcache;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author 愆凡
+ * @date 2021/3/14 14:53
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class EhcacheApplicationTest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhcacheApplicationTest.class, args);
+	}
+
+}
