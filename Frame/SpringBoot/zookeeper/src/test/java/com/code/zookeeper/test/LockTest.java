7ff327f578dd66eb97b744014e958ac88de2e221
zgw <zgwmail@icloud.com>
2021-03-23 10:56:06 +0800

    ZooKeeper - Lock

--- Frame/SpringBoot/zookeeper/src/test/java/com/code/zookeeper/test/LockTest.java
+++ Frame/SpringBoot/zookeeper/src/test/java/com/code/zookeeper/test/LockTest.java
@@ -2,6 +2,11 @@ package com.code.zookeeper.test;
 
 import com.code.zookeeper.ZookeeperApplicationTest;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import org.junit.Before;
 
 /**
  * @author 愆凡
@@ -9,4 +14,27 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class LockTest extends ZookeeperApplicationTest {
+
+	public static final String ZOOKEEPER_ADDERS = "linux.notuptoyou.site:12181";
+	public static final String NAME_SPACE = "zkLockTest";
+	public static final String NODE_PER_CODE = "/";
+	public static CuratorFramework client = null;
+
+	@Before
+	public void init() {
+		client = CuratorFrameworkFactory.builder()
+				.connectString(ZOOKEEPER_ADDERS)
+				.namespace(NAME_SPACE)
+				.sessionTimeoutMs(5000)
+				.connectionTimeoutMs(5000)
+				.retryPolicy(new ExponentialBackoffRetry(1000, 3))
+				.build();
+
+		client.start();
+	}
+
+	public void tryLock() throws Exception {
+		client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath("/test-4");
+	}
+
 }
