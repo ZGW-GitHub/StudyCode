d3958b717b773d3e58391283154428bfebcc38db
zgw <zgwmail@icloud.com>
2021-04-02 09:36:48 +0800

    Java - Math

--- Java/src/main/java/com/code/base/math/MathTest.java
+++ Java/src/main/java/com/code/base/math/MathTest.java
@@ -15,16 +15,16 @@ public class MathTest {
 	 */
 	@Test
 	public void formatTest() {
-		System.err.println("- 方式1 ------>  " + String.format("%.2f", 0.300));
+		System.err.println("- 方式1 ------>  " + String.format("%.2f", 0.395));
 
 		// 对于整数部分，若整数部分为0 ：# 表示最多有几位（因此时整数为0所以认为整数没有） ，0 表示必须有几位
 		// 对于整数部分，若整数部分不为0 ：无论是 # 或 0 ，整数部分将显示全
 		// 对于小数部分：# 表示最多有几位 ，0 表示必须有几位
-		System.err.println("- 方式2 ------>  " + new DecimalFormat("0.00").format(0.300));
-		System.err.println("- 方式2 ------>  " + new DecimalFormat("0.##").format(0.300));
+		System.err.println("- 方式2 ------>  " + new DecimalFormat("0.00").format(0.395));
+		System.err.println("- 方式2 ------>  " + new DecimalFormat("0.##").format(0.395));
 
-		System.err.println("- 方式3 ------>  " + new DecimalFormat("#.00").format(0.355));
-		System.err.println("- 方式3 ------>  " + new DecimalFormat("0.00").format(0.355));
+		System.err.println("- 方式3 ------>  " + new DecimalFormat("#.00").format(0.395));
+		System.err.println("- 方式3 ------>  " + new DecimalFormat("0.00").format(0.395));
 	}
 
 }
