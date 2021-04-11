82824f49d160ae7bd7622ab97c1ed27cfb1b0542
愆凡 <zgwmail@icloud.com>
2021-04-11 22:42:41 +0800

    Spring Cloud - Config

--- Frame/SpringCloud/service-web/src/main/java/com/code/service/web/controller/OtherController.java
+++ Frame/SpringCloud/service-web/src/main/java/com/code/service/web/controller/OtherController.java
@@ -1,6 +1,7 @@
 package com.code.service.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +16,17 @@ public class OtherController {
 	@Autowired
 	private DiscoveryClient discoveryClient;
 
+	@Value("${config:nothing}")
+	private String config;
+
 	@GetMapping("/getDiscovery")
 	public DiscoveryClient getDiscovery() {
 		return discoveryClient;
+	}
+
+	@GetMapping("/getConfig")
+	public String getConfig() {
+		return config;
 	}
 
 }
