5ac0eeda3dd7e40a50a91456be404a49d049ff0c
愆凡 <zgwmail@icloud.com>
2021-04-11 20:33:50 +0800

    Spring Cloud - GateWay

--- Frame/SpringCloud/component-gateway/src/main/java/com/code/gateway/GatewayApplication.java
+++ Frame/SpringCloud/component-gateway/src/main/java/com/code/gateway/GatewayApplication.java
@@ -2,11 +2,13 @@ package com.code.gateway;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 愆凡
  * @date 2021/4/7 17:05
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
