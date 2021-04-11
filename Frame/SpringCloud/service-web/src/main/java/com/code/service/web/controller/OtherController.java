a6f9431f4519bcf8280683970099cdd8b23e43bb
愆凡 <zgwmail@icloud.com>
2021-04-11 23:10:33 +0800

    Spring Cloud - Config

--- Frame/SpringCloud/service-web/src/main/java/com/code/service/web/controller/OtherController.java
+++ Frame/SpringCloud/service-web/src/main/java/com/code/service/web/controller/OtherController.java
@@ -3,6 +3,7 @@ package com.code.service.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author 愆凡
  * @date 2021/4/11 15:00
  */
+@RefreshScope
 @RestController
 public class OtherController {
 
