86313ec09269f8b4838080454c88744a48bb260a
愆凡 <zgwmail@icloud.com>
2021-04-11 21:29:58 +0800

    Spring Cloud - GateWay 动态路由进行负载均衡

---
+++ Frame/SpringCloud/component-gateway/src/main/java/com/code/gateway/filter/MyLogGlobalFilter.java
@@ -0,0 +1,38 @@
+package com.code.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Random;
+
+/**
+ * 自定义全局Filter，用来记录日志
+ *
+ * @author 愆凡
+ * @date 2021/4/11 21:12
+ */
+@Component
+public class MyLogGlobalFilter implements GlobalFilter, Ordered {
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		System.err.println("ServerWebExchange : " + exchange.toString());
+		System.err.println("GatewayFilterChain : " + chain.toString());
+
+		if (new Random().nextBoolean()) {
+			System.err.println("程序错误");
+			return exchange.getResponse().setComplete();
+		}
+		return chain.filter(exchange);
+	}
+
+	@Override
+	public int getOrder() {
+		return 0;
+	}
+
+}
