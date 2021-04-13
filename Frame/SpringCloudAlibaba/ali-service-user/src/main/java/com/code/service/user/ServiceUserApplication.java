ccd4e27f46d45653f757b3686b25fced60449b4d
愆凡 <zgwmail@icloud.com>
2021-04-13 22:16:53 +0800

    Spring Cloud Alibaba - Nacos

--- Frame/SpringCloudAlibaba/ali-service-user/src/main/java/com/code/service/user/ServiceUserApplication.java
+++ Frame/SpringCloudAlibaba/ali-service-user/src/main/java/com/code/service/user/ServiceUserApplication.java
@@ -3,12 +3,14 @@ package com.code.service.user;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 愆凡
  * @date 2021/4/7 18:26
  */
 @EnableDubbo(scanBasePackages = {"com.code.service.user"})
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceUserApplication {
 
--- Frame/SpringCloudAlibaba/ali-service-web/src/main/java/com/code/service/web/ServiceWebApplication.java
+++ Frame/SpringCloudAlibaba/ali-service-web/src/main/java/com/code/service/web/ServiceWebApplication.java
@@ -3,12 +3,14 @@ package com.code.service.web;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 愆凡
  * @date 2021/4/7 18:28
  */
 @EnableDubbo(scanBasePackages = {"com.code.service.web"})
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceWebApplication {
 
