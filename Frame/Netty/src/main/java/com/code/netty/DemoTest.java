bb5a72b56fa1e05d1073b104dd6284da1d67de71
愆凡 <zgwmail@icloud.com>
2021-04-16 18:10:22 +0800

    Netty

--- Frame/Netty/src/main/java/com/code/netty/DemoTest.java
+++ Frame/Netty/src/main/java/com/code/netty/DemoTest.java
@@ -1,5 +1,6 @@
 package com.code.netty;
 
+import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -7,19 +8,22 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.concurrent.Future;
+import org.junit.Test;
 
 /**
  * @author 愆凡
+ * @date 2021/04/16 6:07 下午
  */
-public class NettyServer {
-	public static void main(String[] args) {
-
+public class DemoTest {
+	
+	@Test
+	public void serverTest() {
 		final EventLoopGroup bossGroup = new NioEventLoopGroup(1);
 		final EventLoopGroup workerGroup = new NioEventLoopGroup();
 
 		try {
-
 			ServerBootstrap bootstrap = new ServerBootstrap()
 					.group(bossGroup, workerGroup)
 					.channel(NioServerSocketChannel.class)
@@ -44,6 +48,32 @@ public class NettyServer {
 			bossFuture.syncUninterruptibly();
 			workerFuture.syncUninterruptibly();
 		}
-
 	}
+	
+	@Test
+	public void clientTest() {
+		EventLoopGroup group = new NioEventLoopGroup();
+		
+		try {
+			Bootstrap bootstrap = new Bootstrap()
+					.group(group)
+					.channel(NioSocketChannel.class)
+					.handler(new ChannelInitializer<SocketChannel>() {
+						@Override
+						protected void initChannel(SocketChannel channel) {
+							channel.pipeline().addLast(new ClientChannelHandler());
+						}
+					});
+
+			ChannelFuture channelFuture = bootstrap.connect("localhost", 28082).sync();
+
+			channelFuture.channel().closeFuture().sync();
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			group.shutdownGracefully();
+		}
+	}
+	
 }
