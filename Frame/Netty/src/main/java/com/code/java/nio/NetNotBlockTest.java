a727bd7a91fcbf5c963decc8ffa0fcc9deb51b02
愆凡 <zgwmail@icloud.com>
2021-04-20 21:43:02 +0800

    Netty

--- Frame/Netty/src/main/java/com/code/java/nio/NetNotBlockTest.java
+++ Frame/Netty/src/main/java/com/code/java/nio/NetNotBlockTest.java
@@ -22,7 +22,7 @@ public class NetNotBlockTest {
 	public void serverTest() {
 		new Server().start();
 	}
-	
+
 	@Test
 	public void clientTest() throws IOException, InterruptedException {
 		Client client = new Client("小明");
@@ -45,25 +45,24 @@ public class NetNotBlockTest {
 		}
 	}
 
+	private static final int PORT = 9999;
+	private static final String HOST = "127.0.0.1";
+
 	static class Server {
-
-		private ServerSocketChannel serverSocketChannel;
-
 		private Selector selector;
-
-		private static final int PORT = 9999;
+		private ServerSocketChannel serverSocketChannel;
 
 		private Server() {
 			try {
-				// 1、获取 ServerSocketChannel 监听所有通道
+				// 1、创建 ServerSocketChannel
 				serverSocketChannel = ServerSocketChannel.open();
-				// 2、获取选择器
+				// 2、创建 Selector
 				selector = Selector.open();
-				// 3、设置阻塞方式
-				serverSocketChannel.configureBlocking(false);
-				// 4、绑定端口
+				// 3、绑定端口
 				serverSocketChannel.bind(new InetSocketAddress(PORT));
-				// 5、将 ServerSocketChannel 注册给选择器
+				// 4、设置为非阻塞（因为一个 Channel 要注册到 Selector 中，Channel 必须是非阻塞的）
+				serverSocketChannel.configureBlocking(false);
+				// 5、将 ServerSocketChannel 注册到 Selector ，并监听新连接事件（ServerSocketChannel 只需要监听新连接事件即可）
 				serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -73,44 +72,47 @@ public class NetNotBlockTest {
 		private void start() {
 			try {
 				while (true) {
+					// 获取新连接事件就绪的 Channel 数量
 					if ((selector.select(2_000)) == 0) {
-						System.out.println("无连接！");
+						System.out.println("无新连接！");
 						continue;
 					}
 
-					Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-					while (iterator.hasNext()) {
-						SelectionKey key = iterator.next();
+					// 获取所有已就绪 Channel 对应的 SelectionKey
+					Iterator<SelectionKey> keys = selector.selectedKeys().iterator();
+					while (keys.hasNext()) {
+						SelectionKey key = keys.next();
 
-						if (key.isAcceptable()) { // 处理连接请求
+						// 处理新连接就绪事件
+						if (key.isAcceptable()) {
+							// a、创建一个 SocketChannel 用来与客户端的 Channel 进行通信 
 							SocketChannel socketChannel = serverSocketChannel.accept();
 							socketChannel.configureBlocking(false);
+							// b、将新建的 SocketChannel 注册到 Selector，并监听读事件
 							socketChannel.register(selector, SelectionKey.OP_READ);
+
 							System.out.println(socketChannel.getRemoteAddress().toString() + " -> 上线了。。。");
 						}
 
-						if (key.isReadable()) { // 处理读请求
-							getMsg(key);
+						// 处理读就绪事件（读取客户端发来的消息）
+						if (key.isReadable()) {
+							// a、通过 SelectionKey 获取与之关联的 Channel
+							SocketChannel socketChannel = (SocketChannel) key.channel();
+							ByteBuffer buffer = ByteBuffer.allocate(1024);
+							int length = socketChannel.read(buffer);
+							if (length > 0) {
+								String msg = new String(buffer.array(), 0, length, StandardCharsets.UTF_8);
+								printInfo(msg);
+								// 广播
+								setAnyBody(msg, socketChannel);
+							}
 						}
 
-						iterator.remove();
+						keys.remove();
 					}
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
-			}
-		}
-
-		// 读取客户端发送的消息，并广播
-		private void getMsg(SelectionKey key) throws IOException {
-			SocketChannel socketChannel = (SocketChannel) key.channel();
-			ByteBuffer buffer = ByteBuffer.allocate(1024);
-			int length = socketChannel.read(buffer);
-			if (length > 0) {
-				String msg = new String(buffer.array(), 0, length, StandardCharsets.UTF_8);
-				printInfo(msg);
-				// 广播
-				setAnyBody(msg, socketChannel);
 			}
 		}
 
@@ -121,10 +123,14 @@ public class NetNotBlockTest {
 
 		// 发广播的方法
 		private void setAnyBody(String msg, SocketChannel channel) throws IOException {
+			// 获取 Selector 所有的 SelectionKey
 			Set<SelectionKey> keys = selector.keys();
+
 			ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes(StandardCharsets.UTF_8));
+			// 遍历
 			for (SelectionKey key : keys) {
 				SelectableChannel selectableChannel = key.channel();
+				// 筛选掉 ServerSocketChannel 和自己
 				if (selectableChannel instanceof SocketChannel && selectableChannel != channel) {
 					SocketChannel c = (SocketChannel) selectableChannel;
 					c.write(buffer);
@@ -137,20 +143,14 @@ public class NetNotBlockTest {
 
 
 	static class Client {
-
-		private static final int PORT = 9999;
-		private static final String HOST = "127.0.0.1";
 		private final String USER_NAME;
 		private final SocketChannel channel;
 
 		public Client(String name) throws IOException, InterruptedException {
-
 			USER_NAME = name;
-
 			InetSocketAddress address = new InetSocketAddress(HOST, PORT);
-
+			// 创建 SocketChannel 用来和服务端通信
 			channel = SocketChannel.open();
-
 			channel.configureBlocking(false);
 
 			if (!channel.connect(address)) {
@@ -160,15 +160,11 @@ public class NetNotBlockTest {
 				}
 			}
 
-			String userAddress = channel.getLocalAddress().toString();
-
-			System.out.println("------- " + userAddress + " 成功登录 -------");
-
+			System.out.println("------- " + channel.getLocalAddress().toString() + " 成功登录 -------");
 		}
 
 		// 发消息
 		public void setMsg(String msg) throws IOException {
-
 			if ("exit".equalsIgnoreCase(msg)) {
 				channel.close();
 				return;
@@ -177,18 +173,15 @@ public class NetNotBlockTest {
 			msg = USER_NAME + " 说：" + msg;
 			ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes(StandardCharsets.UTF_8));
 			channel.write(buffer);
-
 		}
 
 		// 获取 Server 返回的消息
 		public void getMsg() throws IOException {
-
 			ByteBuffer buffer = ByteBuffer.allocate(1024);
 			int length = channel.read(buffer);
 			if (length > 0) {
 				System.out.println(new String(buffer.array(), 0, length, StandardCharsets.UTF_8));
 			}
-
 		}
 
 	}
