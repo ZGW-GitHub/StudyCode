938957cb7dafc3470d738bbd1f5e7384e05ce686
愆凡 <zgwmail@icloud.com>
2021-04-29 14:10:57 +0800

    Java Thread

--- Java/src/main/java/com/code/thread/nn/threadpool/basic/oo/runnable/scheduled/future/RunnableScheduledFutureTest.java
+++ Java/src/main/java/com/code/thread/nn/threadpool/basic/oo/runnable/scheduled/future/RunnableScheduledFutureTest.java
@@ -1,21 +1,18 @@
 package com.code.thread.nn.threadpool.basic.oo.runnable.scheduled.future;
 
-import org.junit.Test;
+import com.code.thread.nn.threadpool.executor.scheduled.threadpoll.executor.ScheduledThreadPoolExecutorTest;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
+ * 是 {@link ScheduledThreadPoolExecutor} 的内部类，作为该线程池的任务单元。<br /><br />
+ * 具体示例参考：{@link ScheduledThreadPoolExecutorTest}<br /><br />
+ *
+ * @see RunnableScheduledFuture
+ * 
  * @author 愆凡
  * @date 2020/5/11 9:14 上午
  */
 public class RunnableScheduledFutureTest {
-
-	private final ExecutorService executor = Executors.newScheduledThreadPool(2);
-
-	@Test
-	public void test() {
-
-	}
 
 }
