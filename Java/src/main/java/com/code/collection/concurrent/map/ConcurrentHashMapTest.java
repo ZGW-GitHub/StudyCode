27474283b3dbf65dc82922f05e2f087e468f4d64
愆凡 <zgwmail@icloud.com>
2021-05-14 15:23:50 +0800

    Java - Collection

--- Java/src/main/java/com/code/collection/concurrent/map/ConcurrentHashMapTest.java
+++ Java/src/main/java/com/code/collection/concurrent/map/ConcurrentHashMapTest.java
@@ -1,8 +1,25 @@
 package com.code.collection.concurrent.map;
 
+import org.junit.After;
+import org.junit.Test;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @author 愆凡
  */
 public class ConcurrentHashMapTest {
+
+	private final ConcurrentHashMap<Integer, Integer> hashMap = new ConcurrentHashMap<>();
+	
+	@Test
+	public void test() {
+		hashMap.put(1, 2);
+	}
+	
+	@After
+	public void after() {
+		hashMap.forEach((k, v) -> System.err.println(k + " --- " + v));
+	}
 
 }
