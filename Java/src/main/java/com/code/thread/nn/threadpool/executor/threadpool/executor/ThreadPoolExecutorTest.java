54c2900a81ffb80002869088613b7974de875178
愆凡 <zgwmail@icloud.com>
2021-05-26 11:44:10 +0800

    Java - ThreadPoolExecutor

--- Java/src/main/java/com/code/thread/nn/threadpool/executor/threadpool/executor/ThreadPoolExecutorTest.java
+++ Java/src/main/java/com/code/thread/nn/threadpool/executor/threadpool/executor/ThreadPoolExecutorTest.java
@@ -1,13 +1,11 @@
 package com.code.thread.nn.threadpool.executor.threadpool.executor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.lang.NonNull;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
@@ -16,9 +14,16 @@ import java.util.stream.IntStream;
  *
  * @author 愆凡
  */
+@Slf4j
 public class ThreadPoolExecutorTest {
 
-	private final ExecutorService executorService = Executors.newFixedThreadPool(10, new MyThreadFactory());
+	private final ExecutorService executorService = new ThreadPoolExecutor(
+			3, 5, 60, TimeUnit.SECONDS,
+			new ArrayBlockingQueue<>(10), new MyThreadFactory(),
+			(runnable, executor) -> {
+				log.warn("线程池已满,任务:" + runnable + ",已被拒绝");
+				// 处理被拒绝的任务
+			});
 
 	/**
 	 * 拦截并处理任务执行过程中的异常
@@ -27,7 +32,7 @@ public class ThreadPoolExecutorTest {
 	 */
 	@Test
 	public void exceptionHandlerTest() throws InterruptedException {
-		executorService.execute(() -> {
+		executorService.submit(() -> {
 			try {
 				TimeUnit.SECONDS.sleep(2);
 			} catch (InterruptedException e) {
@@ -35,7 +40,7 @@ public class ThreadPoolExecutorTest {
 			}
 		});
 
-		IntStream.range(0, 10).boxed().forEach(integer -> executorService.execute(() -> System.out.println(1 / 0)));
+		IntStream.range(0, 10).boxed().forEach(integer -> executorService.submit(() -> System.out.println(1 / 0)));
 
 		executorService.shutdown();
 		Assert.assertTrue(executorService.awaitTermination(10, TimeUnit.SECONDS));
@@ -44,19 +49,15 @@ public class ThreadPoolExecutorTest {
 	}
 
 	private static class MyThreadFactory implements ThreadFactory {
-		private final AtomicInteger SEQ = new AtomicInteger();
+		private final AtomicInteger threadNum = new AtomicInteger();
 
 		@Override
 		public Thread newThread(@NonNull Runnable runnable) {
 			Thread thread = new Thread(runnable);
-			thread.setName("MyThread-" + SEQ.getAndIncrement());
-
+			thread.setName("MyThread-" + threadNum.getAndIncrement());
 			// 设置异常处理程序
-			thread.setUncaughtExceptionHandler((athread, cause) -> {
-				System.out.println("The Thread : " + athread.getName() + " 执行失败！");
-				// 输出堆栈信息
-//				cause.printStackTrace();
-			});
+			thread.setUncaughtExceptionHandler((athread, cause) ->
+					log.error("The Thread : " + athread.getName() + " 执行失败！", cause));
 
 			return thread;
 		}
