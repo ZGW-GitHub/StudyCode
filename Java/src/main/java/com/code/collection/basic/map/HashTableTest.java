6bba79a8fe0cf40d47dcb37e9346d7b431fdb625
愆凡 <zgwmail@icloud.com>
2021-05-28 14:42:27 +0800

    Java

---
+++ Java/src/main/java/com/code/collection/basic/map/HashTableTest.java
@@ -0,0 +1,16 @@
+package com.code.collection.basic.map;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Hashtable;
+
+/**
+ * @author 愆凡
+ * @date 2021/5/28 14:41
+ */
+@Slf4j
+public class HashTableTest {
+
+	private final Hashtable<Integer, Integer> hashtable = new Hashtable<>();
+	
+}
