dac962ae320ecac836b6aee948d8d32e458f5a4e
愆凡 <zgwmail@icloud.com>
2021-06-01 10:10:33 +0800

    Java

---
+++ Java/src/main/java/com/code/collection/basic/list/LinkedListTest.java
@@ -0,0 +1,22 @@
+package com.code.collection.basic.list;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+/**
+ * @author 愆凡
+ * @date 2021/6/1 10:07
+ */
+@Slf4j
+public class LinkedListTest {
+
+	private final LinkedList<Integer> linkedList = new LinkedList<>();
+
+	@Test
+	public void test() {
+
+	}
+
+}
