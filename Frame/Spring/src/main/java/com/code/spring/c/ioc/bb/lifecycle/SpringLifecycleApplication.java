25ebd88e499dd066f9896f79857631cbe436b9c5
愆凡 <zgwmail@icloud.com>
2021-06-18 16:42:03 +0800

    Spring Lifecycle

--- Frame/Spring/src/main/java/com/code/spring/c/ioc/bb/lifecycle/SpringLifecycleApplication.java
+++ Frame/Spring/src/main/java/com/code/spring/c/ioc/bb/lifecycle/SpringLifecycleApplication.java
@@ -13,7 +13,25 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class SpringLifecycleApplication {
 	public static void main(String[] args) {
 
-		new SpringApplicationBuilder(SpringLifecycleApplication.class).run(args);
+//		test(args);
+
+		initPropertySourcesTest(args);
 
 	}
+
+	private static void test(String[] args) {
+		new SpringApplicationBuilder(SpringLifecycleApplication.class).run(args);
+	}
+
+	/**
+	 * 测试生命周期方法(initPropertySources)的扩展
+	 *
+	 * @param args args
+	 */
+	private static void initPropertySourcesTest(String[] args) {
+		new SpringApplicationBuilder(SpringLifecycleApplication.class)
+				.main(MyAnnotationConfigApplicationContext.class)
+				.run(args);
+	}
+
 }
