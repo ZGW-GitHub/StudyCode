efba6e0e543d018106592a6068b9acbd6b7ed3f8
愆凡 <zgwmail@icloud.com>
2021-06-24 20:01:52 +0800

    Spring Bean Lifecycle

---
+++ Frame/Spring/src/main/java/com/code/spring/c/ioc/cc/lifecycle/MyAnnotationConfigApplicationContext.java
@@ -0,0 +1,22 @@
+package com.code.spring.c.ioc.cc.lifecycle;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+
+/**
+ * @author 愆凡
+ * @date 2021/6/18 16:32
+ */
+@Slf4j
+public class MyAnnotationConfigApplicationContext extends AnnotationConfigServletWebServerApplicationContext {
+
+	/**
+	 * 重写方法，设置 test 为必需的属性
+	 */
+	@Override
+	protected void initPropertySources() {
+		super.initPropertySources();
+		getEnvironment().setRequiredProperties("test");
+	}
+
+}
---
+++ Frame/Spring/src/main/java/com/code/spring/c/ioc/cc/lifecycle/SpringLifecycleApplication.java
@@ -0,0 +1,36 @@
+package com.code.spring.c.ioc.cc.lifecycle;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * @author 愆凡
+ * @date 2021/6/18 15:50
+ */
+@Slf4j
+@SpringBootApplication(scanBasePackages = {"com.code.spring.a.basic", "com.code.spring.c.ioc.cc.lifecycle"})
+public class SpringLifecycleApplication {
+	public static void main(String[] args) {
+
+//		test(args);
+		initPropertySourcesTest(args);
+
+	}
+
+	private static void test(String[] args) {
+		new SpringApplicationBuilder(SpringLifecycleApplication.class).run(args);
+	}
+
+	/**
+	 * 测试生命周期方法(initPropertySources)的扩展
+	 *
+	 * @param args args
+	 */
+	private static void initPropertySourcesTest(String[] args) {
+		new SpringApplicationBuilder(SpringLifecycleApplication.class)
+				.contextClass(MyAnnotationConfigApplicationContext.class)
+				.run(args);
+	}
+
+}
