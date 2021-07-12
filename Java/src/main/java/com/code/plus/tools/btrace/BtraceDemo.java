4fa02d459d8e408fa423c9672df9a6378f7ec98e
愆凡 <zgwmail@icloud.com>
2021-07-12 14:06:00 +0800

    BTrace

---
+++ Java/src/main/java/com/code/plus/tools/btrace/BtraceDemo.java
@@ -0,0 +1,22 @@
+package com.code.plus.tools.btrace;
+
+import org.openjdk.btrace.core.BTraceUtils;
+import org.openjdk.btrace.core.annotations.*;
+
+/**
+ * @author 愆凡
+ * @date 2021/7/12 11:59
+ */
+@BTrace
+public class BtraceDemo {
+	
+	@OnMethod(clazz = "com.code.plus.tools.btrace.RunnerDemo", 
+			method = "sleep", 
+			location = @Location(Kind.RETURN))
+	public static void test(@Return long result, @Duration long time, long arg1) {
+		BTraceUtils.print("arg1 : " + arg1);
+		BTraceUtils.print("result : " + result);
+		BTraceUtils.print("time : " + time);
+	}
+	
+}
