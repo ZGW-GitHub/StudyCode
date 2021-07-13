7ebaa717cc62cf88d883add53e6a4cc601660724
愆凡 <zgwmail@icloud.com>
2021-07-13 23:58:30 +0800

    [66]加一

--- Basis/DataStructure/src/main/java/leetcode/x100/Demo66.java
+++ Basis/DataStructure/src/main/java/leetcode/x100/Demo66.java
@@ -55,4 +55,19 @@ public class Demo66 {
 		return doPlusOne(digits, index - 1);
 	}
 
+	public int[] two(int[] digits) {
+		for (int i = digits.length - 1; i >= 0; i--) {
+			if (digits[i] != 9) {
+				digits[i] += 1;
+				return digits;
+			}
+
+			digits[i] = 0;
+		}
+
+		int[] newDigits = new int[digits.length + 1];
+		newDigits[0] = 1;
+		return newDigits;
+	}
+
 }
