5903e488184e7d81072efed3191b180ca49647f6
愆凡 <zgwmail@icloud.com>
2021-07-15 18:07:26 +0800

    [88]合并两个有序数组

--- Basis/DataStructure/src/main/java/leetcode/x100/Demo88.java
+++ Basis/DataStructure/src/main/java/leetcode/x100/Demo88.java
@@ -1,18 +1,34 @@
 package leetcode.x100;
 
+import code.sort.quick.QuickSort;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.Arrays;
 
 /**
  * 合并两个有序数组
- * 
+ *
  * @author 愆凡
  * @date 2021/7/15 17:52
  */
 @Slf4j
 public class Demo88 {
 
-	public void one(int[] nums1, int m, int[] nums2, int n) {
-		
+	@Test
+	public void test() {
+		one(new int[]{1, 5, 2}, 3, new int[]{6, 3}, 2);
 	}
-	
+
+	public void one(int[] nums1, int m, int[] nums2, int n) {
+		int[] result = new int[m + n];
+
+		System.arraycopy(nums1, 0, result, 0, m);
+		System.arraycopy(nums2, 0, result, m, n);
+
+		QuickSort.quickSort(result);
+
+		System.out.println(Arrays.toString(result));
+	}
+
 }
