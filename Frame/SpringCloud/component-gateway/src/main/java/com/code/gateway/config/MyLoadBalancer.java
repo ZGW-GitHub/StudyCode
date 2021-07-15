0d4d3cfc7257fa8db9dd1c01f124606e06aae4d0
愆凡 <zgwmail@icloud.com>
2021-07-15 15:09:25 +0800

    SpringCloud

--- Frame/SpringCloud/component-gateway/src/main/java/com/code/gateway/config/MyLoadBalancer.java
+++ Frame/SpringCloud/component-gateway/src/main/java/com/code/gateway/config/MyLoadBalancer.java
@@ -1,4 +1,4 @@
-package com.code.gateway.filter;
+package com.code.gateway.config;
 
 import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.AbstractLoadBalancerRule;
@@ -6,11 +6,13 @@ import com.netflix.loadbalancer.Server;
 import lombok.extern.slf4j.Slf4j;
 
 /**
+ * 负载均衡
+ * 
  * @author 愆凡
  * @date 2021/7/6 17:53
  */
 @Slf4j
-public class LoadBalancerFilter extends AbstractLoadBalancerRule {
+public class MyLoadBalancer extends AbstractLoadBalancerRule {
 	@Override
 	public void initWithNiwsConfig(IClientConfig iClientConfig) {
 
